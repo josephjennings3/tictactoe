bb712445c53a2b65ad0ab26b4e73ea9094539532
lakazepo <jajenningsiii@gmail.com>
2018-08-15 15:05:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Box from './box.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Box />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/box.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+	render() {
+		return(
+			<div>
+				<section>
+		          <div className="box">
+		            hi
+		          </div>
+		          <div className="box">
+		            hi
+		          </div>
+		          <div className="box">
+		            hi
+		          </div>
+		        </section>
+		        <section>
+		          <div className="box">
+		            hi
+		          </div>
+		          <div className="box">
+		            hi
+		          </div>
+		          <div className="box">
+		            hi
+		          </div>
+		        </section>
+		        <section>
+		          <div className="box">
+		            hi
+		          </div>
+		          <div className="box">
+		            hi
+		          </div>
+		          <div className="box">
+		            hi
+		          </div>
+       			</section>
+       		</div>
+		)
+	}
+}
+
+export default Box;
