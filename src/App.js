d18e5a99f50423a5e02617143b152619191968dd
lakazepo <jajenningsiii@gmail.com>
2018-08-17 13:25:35 -0700

    started project over and following alongside youtube video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-import Box from './box.js';
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      board : Array(9).fill(null)
+    }
+  }
+
+  handleClick(e){
+    console.log(e.target)
+  }
+
   render() {
+    const Box = this.state.board.map(
+      (box, index) => 
+      <div className="box" 
+      key={index} 
+      onClick={(e)=>this.handleClick(e)}>
+      {box}
+      </div>
+    )
     return (
-      <div>
-        <Box />
+      <div className="container">
+        <h1>Tic Tac Toe App</h1>
+        <div className="board">
+          {Box}
+        </div>
       </div>
     );
   }
