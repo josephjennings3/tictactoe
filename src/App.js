651d6d387fc65dfa81010dfe5467b360acdce9bb
lakazepo <jajenningsiii@gmail.com>
2018-08-19 15:31:13 -0700

    organized code a bit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Player from './components/choosePlayer';
 
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     super(props)
     this.state = {
       board : Array(9).fill(null),
-      player: "X"
+      player: null,
+      winner: null,
     }
   }
 
@@ -15,52 +17,68 @@ class App extends Component {
     let winLines = 
     [
       ["0", "1", "2"],
-      ["3", "4", "5",],
-      ["6", "7", "8",],
-      ["1", "4", "7",],
-      ["2", "5", "8",],
-      ["0", "4", "8",],
-      ["2", "4", "6",],
+      ["3", "4", "5"],
+      ["6", "7", "8"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["0", "4", "8"],
+      ["2", "4", "6"],
+      ["0", "3", "6"],
     ]
 
     for (let index = 0; index < winLines.length; index++){
       const [a,b,c] = winLines[index];
       if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]){
         alert('YOU WON');
+        this.setState({
+          winner: this.state.player
+        })
       }
     }
   }
 
   handleClick(index){
-    let newBoard = this.state.board
+    if(this.state.player && !this.state.winner) {
+      let newBoard = this.state.board
 
-    if (this.state.board[index] === null){
-      newBoard[index] = this.state.player 
-      this.setState({
-      board: newBoard,
-      player: this.state.player === "X" ? "O" : "X"
+      if (this.state.board[index] === null){
+        newBoard[index] = this.state.player 
+        this.setState({
+          board: newBoard,
+          player: this.state.player === "X" ? "O" : "X"
+        })
+      }
+      this.checkWinner() 
+    } 
+  }
+
+  setPlayer(player){
+    this.setState({
+      player
     })
+  }
 
-      this.checkWinner()
-    }
-
-    
+  renderBoxes() {
+    return this.state.board.map(
+      (box, index) => 
+      <div className="box" key={index} 
+        onClick={()=>this.handleClick(index)}>
+        {box} </div>
+    )
   }
 
   render() {
-    const Box = this.state.board.map(
-      (box, index) => 
-      <div className="box" 
-      key={index} 
-      onClick={()=>this.handleClick(index)}>
-      {box}
-      </div>
-    )
+    let status = this.state.player ? 
+      <h2>Next Player Is {this.state.player}</h2> : 
+      <Player player={(e) => this.setPlayer(e)} />
+
+
     return (
       <div className="container">
         <h1>Tic Tac Toe App</h1>
+        {status}
         <div className="board">
-          {Box}
+          {this.renderBoxes()}
         </div>
       </div>
     );
